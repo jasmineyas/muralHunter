4c02055f6c702b46be9611bae617d5ebdae82fdc
Nicoleta Bostan <nbostan@students.cs.ubc.ca>
2025-01-19 10:42:50 -0800

    moved seed data to its own file

---
+++ place-guesser/backend/seed_data.js
@@ -0,0 +1,57 @@
+export const seedData = [
+  {
+    latitude: 49.27650343461371,
+    longitude: -123.13107557839683,
+    url: 'https://i.imgur.com/IjjrJrT.jpeg',
+    description:
+      'This striking mural features tall, textured silhouettes of human figures, integrated into the architectural columns of the building. The golden, almost weathered texture of the figures contrasts against the dark backdrop, creating a dramatic and reflective visual experience. The artwork appears to explore themes of connection, identity, and the human condition while blending seamlessly with the surrounding urban environment.',
+  },
+  {
+    latitude: 49.266137,
+    longitude: -123.24789,
+    url: 'https://i.imgur.com/hqBnI3y.jpeg',
+    description: 'UBC Museum of Anthropology',
+  },
+  {
+    latitude: 49.28440191669053,
+    longitude: -123.11432853974921,
+    url: 'https://i.imgur.com/x8KgIEa.jpeg',
+    description: 'Science World',
+  },
+  {
+    latitude: 49.25798463452297,
+    longitude: -123.10086773750527,
+    url: 'https://i.imgur.com/yoFIz57.jpeg',
+    description: 'BC Place',
+  },
+  {
+    latitude: 49.27031694014467,
+    longitude: -123.09902555769969,
+    url: 'https://i.imgur.com/rQGdIfc.jpeg',
+    description: 'Rogers Arena',
+  },
+  {
+    latitude: 49.262438985068464,
+    longitude: -123.10064237780615,
+    url: 'https://i.imgur.com/e86vAOr.jpeg',
+    description: 'Yaletown',
+  },
+  {
+    latitude: 49.27475692799478,
+    longitude: -123.07066978320995,
+    url: 'https://i.imgur.com/MVwBzSQ.jpeg',
+    description: 'Commercial Drive',
+  },
+  {
+    latitude: 49.27042843811737,
+    longitude: -123.09917472065352,
+    url: 'https://i.imgur.com/yfgFgok.jpeg',
+    description: 'Chinatown',
+  },
+  {
+    latitude: 49.26731330635209,
+    longitude: -123.10133716248448,
+    url: 'https://i.imgur.com/zKGNAtd.jpeg',
+    description: 'Gastown',
+  },
+];
